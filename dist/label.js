ddcfdfad47ddd5d01d8512dfef8b1c05aea5bda1
Julian Cox <julian.cox@webspeed.co.nz>
2024-05-10 16:09:33 +1200

    Update to version v2.0.3 in plugins/label

--- dist/label.js
+++ dist/label.js
@@ -1,11 +1,11 @@
-const a = (o, e = {}, t = "") => {
-  const n = document.createElement(o);
-  for (let l in e)
-    n.setAttribute(l, e[l]);
-  return n.innerHTML = t, n;
-}, s = (o, e) => (document.getElementById(o) || document.getElementsByTagName("head")[0].prepend(a("STYLE", { type: "text/css" }, e)), !0), r = function(o = {}) {
-  let e, t, n, l;
-  s("geocam-compass-label", `
+const r = (o, e = {}, n = "") => {
+  const l = document.createElement(o);
+  for (let t in e)
+    l.setAttribute(t, e[t]);
+  return l.innerHTML = n, l;
+}, d = (o, e) => (document.getElementById(o) || document.getElementsByTagName("head")[0].prepend(r("STYLE", { type: "text/css" }, e)), !0), b = function(o = {}) {
+  let e, n, l, t;
+  d("geocam-compass-label", `
   .geocam-label {
     background-color: rgba(255,255,255,0.5);
     padding: 0.25em 0.5em;
@@ -17,33 +17,39 @@ const a = (o, e = {}, t = "") => {
   .geocam-label-empty {
     background-color: rgba(255,255,255,0);
   }
-  `), this.init = function(i) {
-    e = i, l = a("DIV", { class: "geocam-label" }), e.addControl(l, "top"), n = e.store("label"), t = n((c) => {
-      c ? (l.innerHTML = c, l.classList.remove("geocam-label-empty")) : (l.innerHTML = "", l.classList.add("geocam-label-empty"));
+  `), this.init = function(s) {
+    e = s, t = r("DIV", { class: "geocam-label" }), e.addControl(t, "top"), l = e.store("label"), n = l((i) => {
+      i ? (t.innerHTML = i, t.classList.remove("geocam-label-empty")) : (t.innerHTML = "", t.classList.add("geocam-label-empty"));
     });
   }, this.destroy = function() {
-    t(), e.wrapper.removeChild(n);
+    n(), e.wrapper.removeChild(l);
   };
 };
-class d extends HTMLElement {
+class u extends HTMLElement {
+  static get observedAttributes() {
+    return ["caption"];
+  }
   constructor() {
     super(), this.plugin = null, console.log("label init");
   }
+  attributeChangedCallback(e, n, l) {
+    console.log("attribute changed", e, l);
+    const t = this, c = function(s, i) {
+      const a = t.parentNode.viewer;
+      a && a.label ? a.stores.label(i) : setTimeout(() => c(s, i), 100);
+    };
+    c(e, l);
+  }
   connectedCallback() {
-    console.log("label connected");
+    console.log("label connected"), this.plugin = new b();
     const e = this.parentNode;
-    e.viewer && e.viewer.plugin ? (this.plugin = new r(), e.viewer.plugin(this.plugin)) : console.error(
-      "GeocamViewerLabel must be a child of GeocamViewer"
-    );
+    this.viewer = e.viewer, this.viewer && this.viewer.plugin ? this.viewer.plugin(this.plugin) : console.error("GeocamViewerLabel must be a child of GeocamViewer");
   }
   disconnectedCallback() {
-    this.plugin = null, console.log("labe disconnected");
+    this.plugin = null, this.viewer = null, console.log("labe disconnected");
   }
 }
-window.customElements.define(
-  "geocam-viewer-label",
-  d
-);
+window.customElements.define("geocam-viewer-label", u);
 export {
-  d as GeocamViewerLabel
+  u as GeocamViewerLabel
 };
